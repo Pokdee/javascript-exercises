1e8f8f3be2b8999baad2e2b4754746394cbdce74
Pokdee <loselife61@gmail.com>
2023-02-19 08:43:29 +0700

    Complete 6tasks

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, num) {
+  const result = array.filter((number) => number !== num);
+  return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let result = 0;
+  for (let i = 0; i < num2 + 1; i++) {
+    result = i + result;
+  }
+  return result;
 };
 
 // Do not edit below this line
